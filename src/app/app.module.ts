e5f22ff9e222ed1bfea50cf692359ac7bef6f248
Charles <cnavarro3196@gmail.com>
2020-07-06 03:07:42 -0600

    feat: update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,41 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DaughterDComponent } from './default/daughter-d/daughter-d.component';
+import { FatherDComponent } from './default/father-d/father-d.component';
+import { GrandpaDComponent } from './default/grandpa-d/grandpa-d.component';
+import { SonDComponent } from './default/son-d/son-d.component';
 import { MaterialModule } from './material.module';
+import { DaughterOPBComponent } from './onpush broken/daughter-opb/daughter-opb.component';
+import { FatherOPBComponent } from './onpush broken/father-opb/father-opb.component';
+import { GrandpaOPBComponent } from './onpush broken/grandpa-opb/grandpa-opb.component';
+import { SonOPBComponent } from './onpush broken/son-opb/son-opb.component';
+import { DaughterOpComponent } from './onpush/daughter-op/daughter-op.component';
+import { FatherOpComponent } from './onpush/father-op/father-op.component';
+import { GrandpaOpComponent } from './onpush/grandpa-op/grandpa-op.component';
+import { SonOpComponent } from './onpush/son-op/son-op.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrandpaDComponent,
+    FatherDComponent,
+    DaughterDComponent,
+    DaughterOpComponent,
+    FatherOpComponent,
+    GrandpaOpComponent,
+    SonOpComponent,
+    SonDComponent,
+    GrandpaOPBComponent,
+    FatherOPBComponent,
+    SonOPBComponent,
+    DaughterOPBComponent
   ],
   imports: [
     AppRoutingModule,
@@ -17,7 +43,10 @@ import { MaterialModule } from './material.module';
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule
   ],
   bootstrap: [AppComponent]
 })
