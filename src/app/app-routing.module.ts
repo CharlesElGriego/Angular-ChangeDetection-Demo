45220e47869d812d5ff0ece727212515716e2483
Charles <cnavarro3196@gmail.com>
2020-07-06 03:07:08 -0600

    feat(routing): add paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GrandpaDComponent } from './default/grandpa-d/grandpa-d.component';
+import { GrandpaOPBComponent } from './onpush broken/grandpa-opb/grandpa-opb.component';
+import { GrandpaOpComponent } from './onpush/grandpa-op/grandpa-op.component';
 
 
 const routes: Routes = [
+  {
+    path: 'default',
+    component: GrandpaDComponent
+  },
+  {
+    path: 'onpush',
+    component: GrandpaOpComponent
+  },
+  {
+    path: 'onpush-broken',
+    component: GrandpaOPBComponent
+  },
   {
     path: '**',
     redirectTo: '',
